38b7d68d4be89f36f01179ec5587153b4e130072
Grant Orchard <go@hashicorp.com>
2020-10-07 16:50:56 +1100

    Initial commit

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+locals {
+  vpc_id = data.terraform_remote_state.terraform-aws-core.outputs.vpc_id
+  subnets = data.terraform_remote_state.terraform-aws-core.outputs.public_subnets
+}
 
+resource random_pet "this" {
+  length = 2
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "7.0.0"
+
+  vpc_id = local.vpc_id
+  subnets = local.subnets
+  cluster_name = random_pet.this.id
+}
